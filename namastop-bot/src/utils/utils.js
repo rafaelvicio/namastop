d743e3e3ba53954be47b7bcb9e72e05513fa588d
Rafael Vicio <rafaelvicio@icloud.com>
2019-02-18 23:11:50 -0300

    Refatorando - Day 1

--- namastop-bot/src/utils/utils.js
+++ namastop-bot/src/utils/utils.js
@@ -1,8 +1,7 @@
 const getUserFromMessage = message => {
   const start = message.indexOf("@");
   const end = message.indexOf(">");
-  const to = message.substring(start + 1, end);
-  return to;
+  return message.substring(start + 1, end);
 };
 
 module.exports = {
--- namastop-bot/test/index.js
+++ namastop-bot/test/index.js
@@ -1,5 +1,9 @@
 const assert = require("assert");
 
-it("should return true", () => {
-  assert.equal(true, true);
+const util = require("../src/utils/utils");
+
+it("Extrair userID from message", () => {
+  const text = "namastop 2222222222222 <@UG9769B8R>";
+  const result = util.getUserFromMessage(text);
+  assert.equal(result, "UG9769B8R");
 });
